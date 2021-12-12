e0a1b601770dfebeca4988b21decee66bcd3acf8
fyrodems <fyrodems@gmail.com>
2021-12-12 14:58:28 +0100

    add buttons for section organization

--- src/components/pages/Home/HomeOrganizations.js
+++ src/components/pages/Home/HomeOrganizations.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Title from "../../common/Title";
 
 const HomeOrganizations = () => {
+
+    useEffect(() => {
+        document.querySelector("#organizations > div.organizations__btns > button:nth-child(1)").focus();
+    });
+
     return (
         <section id="organizations" className="organizations">
             <Title title="Komu pomagamy?"/>
             <div className="organizations__btns">
-
+                <button className="button">Fundacjom</button>
+                <button className="button">Organizacjom pozarządowym</button>
+                <button className="button">Lokalnym zbiórkom</button>
             </div>
+            
         </section>
     );
 };
---
+++ src/data/organizations and foundations.js
@@ -0,0 +1,26 @@
+export const foundations = [
+    {name: `Fundacja “Dbam o zdrowie”`, aim: `Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.`, donation: `ubrania, jedzenie, sprzęt AGD, meble, zabawki`},
+    {name: `Fundacja “Dla dzieci”`, aim: `Cel i misja: Pomoc dzieciom z ubogich rodzin.`, donation: `ubrania, meble, zabawki`},
+    {name: `Fundacja “Bez domu”`, aim: `Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.`, donation: `ubrania, jedzenie, ciepłe koce`},
+    {name: `Fundacja “Lorem Ipsum 4”`, aim: `Cel i misja: Pomoc dzieciom z ubogich rodzin.`, donation: `lorem, ipsum, dolor, sit, amet`},
+    {name: `Fundacja “Lorem Ipsum 5”`, aim: `Cel i misja: Quis varius quam quisque id diam vel quam elementum pulvinar.`, donation: `egestas, sed, tempus`},
+    {name: `Fundacja “Lorem Ipsum 6”`, aim: `Cel i misja: Scelerisque in dictum non consectetur a erat nam.`, donation: `lorem, ipsum, dolor, sit, amet`},
+    {name: `Fundacja “Lorem Ipsum 7”`, aim: `Cel i misja: Scelerisque in dictum non consectetur a erat nam.`, donation: `ut, aliquam, purus, sit, amet`},
+    {name: `Fundacja “Lorem Ipsum 8”`, aim: `Cel i misja: Quis varius quam quisque id diam vel quam elementum pulvinar.`, donation: `egestas, sed, tempus`},
+    {name: `Fundacja “Lorem Ipsum 9”`, aim: `Cel i misja: Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.`, donation: `lorem, ipsum, dolor, sit, amet`}
+];
+
+export const organizations = [
+    {name: `Organizacja “Lorem Ipsum 1”`, aim: `Cel i misja: Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.`, donation: `lorem, ipsum, dolor, sit, amet`},
+    {name: `Organizacja “Lorem Ipsum 2”`, aim: `Cel i misja: Quis varius quam quisque id diam vel quam elementum pulvinar.`, donation: `egestas, sed, tempus`},
+    {name: `Organizacja “Lorem Ipsum 3”`, aim: `Cel i misja: Scelerisque in dictum non consectetur a erat nam.`, donation: `ut, aliquam, purus, sit, amet`},
+    {name: `Organizacja “Lorem Ipsum 4”`, aim: `Cel i misja: Scelerisque in dictum non consectetur a erat nam.`, donation: `egestas, sed, tempus`},
+    {name: `Organizacja “Lorem Ipsum 5”`, aim: `Cel i misja: Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.`, donation: `ut, aliquam, purus, sit, amet`},
+    {name: `Organizacja “Lorem Ipsum 6”`, aim: `Cel i misja: Quis varius quam quisque id diam vel quam elementum pulvinar.`, donation: `lorem, ipsum, dolor, sit, amet`}
+];
+
+export const foundraising = [
+    {name: `Zbiórka “Lorem Ipsum 1”`, aim: `Cel i misja: Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.`, donation: `lorem, ipsum, dolor, sit, amet`},
+    {name: `Zbiórka “Lorem Ipsum 2”`, aim: `Cel i misja: Scelerisque in dictum non consectetur a erat nam.`, donation: `ut, aliquam, purus, sit, amet`},
+    {name: `Zbiórka “Lorem Ipsum 3”`, aim: `Cel i misja: Quis varius quam quisque id diam vel quam elementum pulvinar.`, donation: `egestas, sed, tempus`}
+]
