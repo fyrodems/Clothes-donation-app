c7b2b5a97716af1e3dbf44454cf243220c90c4dc
fyrodems <fyrodems@gmail.com>
2021-12-12 19:12:51 +0100

    add styles for organization section

--- src/components/pages/Home/HomeOrganizations.js
+++ src/components/pages/Home/HomeOrganizations.js
@@ -1,21 +1,50 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Title from "../../common/Title";
+import {foundations} from "../../../data/organizations and foundations";
 
 const HomeOrganizations = () => {
 
-    useEffect(() => {
-        document.querySelector("#organizations > div.organizations__btns > button:nth-child(1)").focus();
-    });
+    // const [hhh, setHHH] = useState(document.activeElement)
+    //
+    // useEffect(() => {
+    //     document.querySelector("#organizations > div.organizations__btns > button:nth-child(1)").focus();
+    //     setHHH(document.activeElement);
+    //
+    // });
+    // //console.log(hhh.innerText)
+    //
+    // const checkActive = () => {
+    //     const checkActiveInterwal = setInterval(() => {
+    //         //const activeElement = hhh.innerText;
+    //         console.log(document.activeElement.innerText)
+    //     }, 500);
+    // }
+
+
 
     return (
-        <section id="organizations" className="organizations">
+        <section id="organizations" className="organizations" >
             <Title title="Komu pomagamy?"/>
-            <div className="organizations__btns">
+            <div className="organizations__btns" >
                 <button className="button">Fundacjom</button>
-                <button className="button">Organizacjom pozarządowym</button>
+                <button className="button" onFocus={(e) => console.log(e.target.innerText)}>Organizacjom pozarządowym</button>
                 <button className="button">Lokalnym zbiórkom</button>
             </div>
-            
+            {/*first*/}
+            <p className="organizations__content">W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.</p>
+            <ul>
+                {foundations.map((elem, index) => {
+                    return <li className="list__element" key={index}>
+                        <div className="element__field">
+                            <span>{elem.name}</span>
+                            <span>{elem.aim}</span>
+                        </div>
+                        <div>
+                            <span>{elem.donation}</span>
+                        </div>
+                    </li>
+                })}
+            </ul>
         </section>
     );
 };
